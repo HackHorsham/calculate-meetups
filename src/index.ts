b831a8e8e16f07be4c9fd58b260ec448a1f80423
Phil Stenning <philstenning@hotmail.com>
2021-06-09 08:26:21 +0100

    tidy comments

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
-// warning!!! the Date() creates a mutable object
-// pass it as a string value or you will get odd results.
-
-function calculateNextEvent(dateAsString: string): Date {
-  let dateToCheck = new Date(dateAsString);
+/**
+ * calculates the next meetup date after the initial Start Date
+ * @param {string} startDateAsString  A valid date as string that can be parsed by the Date function.
+ * @returns {Date}
+ */
+function calculateNextEvent(startDateAsString: string): Date {
+  let dateToCheck = new Date(startDateAsString);
 
   if (dateToCheck.getDate() >= 14) {
     const month = dateToCheck.getMonth();
-    
-    // reset the day to the first day.
+    /**
+     * reset the day to the first day.
+     */
     dateToCheck.setDate(1);
-    // we have already had this months event
-    //so we need to increment the month.
-    // if the month is December it will roll over year
-    // and set the month to January.
+    /**
+     *  we have already had this months event
+     *  so we need to increment the month.
+     *  if the month is December it will roll over year
+     *  and set the month to January.
+     */
     dateToCheck.setMonth(month + 1);
   }
-  //  console.log(dateToCheck.getDate())
   for (let d = dateToCheck.getDate(); d <= 14; d++) {
     dateToCheck.setDate(d);
     if (dateToCheck.getDate() > 7 && dateToCheck.getDay() === 6) {
@@ -25,23 +29,27 @@ function calculateNextEvent(dateAsString: string): Date {
   }
   return dateToCheck;
 }
-// console.log(calculateNextEvent('aug 22 2020'))
-
-const getFutureEvents = (dateAsString: string, futureEventsQuantity = 1) => {
+/**
+ * Automatically calculate the  dates for the for events after the initial start date.
+ * @param {string} startDateAsString - A valid date as string that can be parsed by the Date function.
+ * @param {number} futureEventsQuantity - an Integer for the amount of dates that you want to be returned
+ * @returns {Date[]}
+ */
+const getFutureEvents = (startDateAsString: string, futureEventsQuantity = 1) => {
   const events: Array<Date> = [];
-  let date = new Date(dateAsString.trim());
+  let date = new Date(startDateAsString.trim());
   for (let n = 0; n < futureEventsQuantity; n++) {
-    // pass in a date string not a Date()
+    /**
+     * pass in a date string not a Date()
+     */
     const event = calculateNextEvent(date.toDateString());
     events.push(event);
-    //increment the month.
+    /**
+     * Increment the month.
+     */
     date.setMonth(date.getMonth() + 1);
   }
   return events;
 };
-
-const events = getFutureEvents("JAn 1 2021", 12);
-
-events.forEach((e) => console.log(e.toString()));
 
 export { getFutureEvents, calculateNextEvent };
