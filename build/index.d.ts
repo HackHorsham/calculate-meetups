ee84fce7edb825f883b37ec464557e61e8f7c5ee
Phil Stenning <philstenning@hotmail.com>
2021-06-25 14:56:04 +0100

    remove unneeded files

--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function calculateNextEvent(startDateAsString: string): Date;
-declare const getFutureEvents: (startDateAsString: string, futureEventsQuantity?: number) => Date[];
-export { getFutureEvents, calculateNextEvent };
